49e83cc580c9ff4218a75974793c38cd31354a9c
妍嘉 程 <107111213@mail.oit.edu.tw>
2021-05-24 16:28:37 +0800

    Quiz20210518

---
+++ Quiz/Quiz20210518.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Quiz
+{
+    public partial class Quiz20210518 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string scon = "Data Source=(localdb)\\ProjectsV13;" +
+            "Initial Catalog=Test;" +
+            "Integrated Security=True;" +
+            "Connect Timeout=30;Encrypt=False;" +
+            "TrustServerCertificate=False;" +
+            "ApplicationIntent=ReadWrite;MultiSubnetFailover=False;" +
+            "User ID = sa; Password = 12345";
+
+            try
+            {
+                SqlConnection dbcon = new SqlConnection(scon);
+                SqlCommand com = new SqlCommand("INSERT INTO Users(Id,Name,Birthday) VALUES(3,N'雅每每','2021/11/11')", dbcon);
+                dbcon.Open();
+                int reader = com.ExecuteNonQuery();
+                if (reader == 1)
+                    Response.Write("Insert a data successfully!");
+                else
+                    Response.Write("Failed!");
+                dbcon.Close();
+            }
+            catch (Exception exc)
+            {
+                Response.Write(exc.ToString());
+            }
+    }
+}
